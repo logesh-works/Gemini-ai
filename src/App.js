73a082a6169ad89d463770cb3e44f46654cefef0
Logesh Kumar <logeshkumar.dev@gmail.com>
2023-12-22 23:08:56 +0530

    @logesh-works

---
+++ src/App.js
@@ -0,0 +1,93 @@
+import './App.css';
+import { useEffect, useState , useRef} from 'react';
+import Badge from 'react-bootstrap/Badge';
+import { GoogleGenerativeAI } from '@google/generative-ai';
+import { FaRegCopy , FaCopy} from "react-icons/fa";
+import copy from 'clipboard-copy';
+import Img from './Img';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+function App() {
+  const [isloading , Setloading] = useState(true)
+  const [response , Setresponse] = useState("");
+  const [currequest , Setcurrequest] = useState("");
+  const [request , Setrequest] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+  const [iscopy, setIscopy] = useState(false);
+  const API_KEY = "AIzaSyAs2stO1DOojDmaxRYwZpVLSvofVh-KYdk"
+  const genAI = new GoogleGenerativeAI(API_KEY);
+  const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+  async function grd(prompt) {
+    try {
+      const result = await model.generateContent(prompt)
+      const response = await result.response;
+      let text = response.text();
+      Setresponse(text);
+    } catch (error) {
+      Setresponse(error.text)
+      console.log(error.text)
+    }
+    finally{
+      Setloading(false)
+    }
+      /* const result = await model.generateContent(prompt)
+      .catch((Error) => { Setresponse() })
+      .finally(() => {Setloading(false)});   */
+      
+  }
+  useEffect(() => {
+    grd(request);
+    
+  },[request])
+  const toggleNav = () => {
+    setIsOpen(!isOpen);
+  };
+  const copynav = () => {
+    if(iscopy != true){
+      setIscopy(true)
+    }
+    else{
+      setIscopy(false)
+    }
+  };
+  const handleCopy = () => {
+    if (response !== "") {
+      copy(response);
+      alert('Text copied to clipboard!');
+    }
+  }; 
+  useEffect(() => {Setresponse("Welcome To Gemini AI ")} , [])
+  return (
+    <>
+    <div id='heading'>
+      <span id="spa1">GEMINI AI </span>
+      <a href='https://linktr.ee/me_logi' style={{textDecoration:"none"}}><span id="spa2">From Logesh</span></a>
+    </div>
+    
+    <button className="toggle-button" onClick={toggleNav}>
+        <Badge>= </Badge>
+      </button>
+    <div className={`slide-nav ${isOpen ? 'open' : ''}`}>
+    <button className="toggle-button" onClick={toggleNav}>
+        <Badge>=</Badge>
+      </button>
+      <ul>
+        <li>Text To Text</li>
+        <li onClick={() => alert("Still under development")}>Image To Text</li>
+        <a href="https://logesh-works.github.io/" style={{textDecoration:"none"}}><li>About Logesh</li></a>
+      </ul>
+    </div>
+    {/* <div className='response'><p>{response}</p></div> */}
+    <textarea className='response' value={(isloading ? "Loading...":response)} readOnly></textarea>
+    <button className='copybtn' onClick={()=>{copynav()
+    handleCopy()}}>{(iscopy ? <FaCopy /> : <FaRegCopy />  )}</button>
+    <textarea className='protext' rows="1" onChange={(e) => {Setcurrequest(e.target.value) 
+}} value={currequest} placeholder='Search Here...'></textarea>
+    <button className='submit' onClick={() => {(currequest === "" ? Setresponse("Please enter some text"):Setrequest(currequest))
+  Setloading(true)}}>Submit</button>
+    
+    </>
+  );
+}
+
+export default App;
---
+++ src/Img.js
@@ -0,0 +1,9 @@
+const Img = () => {
+    return(
+        <>
+        <h1>Still under the devlopement... </h1>
+        </>
+    )
+}
+
+export default Img;
