2d5fdea094bb452605ebac7ffdd0c00c8bc1ee66
Logesh Kumar <130851416+logesh-works@users.noreply.github.com>
2024-06-17 22:31:18 +0530

    Update TextToText.js

--- src/TextToText.js
+++ src/TextToText.js
@@ -12,7 +12,7 @@ const TextToText = () => {
     const [request , Setrequest] = useState(null);
     const [isOpen, setIsOpen] = useState(false);
     const [iscopy, setIscopy] = useState(false);
-    const API_KEY = "AIzaSyC9K7yD9hfvhSG26gO0jEHKc0JQhJsIPQE"
+    const API_KEY = "AIzaSyC_8SD5pDg6-ZG9Azr7pDuGarlT_EbqGhI"
     const genAI = new GoogleGenerativeAI(API_KEY);
     const model = genAI.getGenerativeModel({ model: "gemini-pro"});
     async function grd(prompt) {
